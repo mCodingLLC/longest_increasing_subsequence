a22558170d63fbae69f8b2a8e21b3640dd20d828
James Murphy <james@mcoding.io>
2021-02-15 13:25:12 -0600

    Bump version: 0.1.2 → 0.1.3

--- longest_increasing_subsequence/__init__.py
+++ longest_increasing_subsequence/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """James Murphy"""
 __email__ = 'james@mcoding.io'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from .implementation import (longest_increasing_subsequence,
                              longest_decreasing_subsequence,
