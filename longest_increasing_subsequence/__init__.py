f449e08e5fa803cbfe4e6bee05fb7d5fce619ba8
James Murphy <james@mcoding.io>
2021-02-16 12:56:05 -0600

    Rename implementation to _implementation to discourage use of non-public implementation details

--- longest_increasing_subsequence/__init__.py
+++ longest_increasing_subsequence/__init__.py
@@ -4,10 +4,10 @@ __author__ = """James Murphy"""
 __email__ = 'james@mcoding.io'
 __version__ = '0.1.4'
 
-from .implementation import (longest_increasing_subsequence,
-                             longest_decreasing_subsequence,
-                             longest_increasing_subsequence_indices,
-                             longest_decreasing_subsequence_indices, )
+from ._implementation import (longest_increasing_subsequence,
+                              longest_decreasing_subsequence,
+                              longest_increasing_subsequence_indices,
+                              longest_decreasing_subsequence_indices, )
 
 __all__ = ['longest_increasing_subsequence',
            'longest_decreasing_subsequence',
